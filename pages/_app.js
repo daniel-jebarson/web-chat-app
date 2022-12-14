68040a0633d516aeefe3755a76abbebdf897b511
daniel-jebarson <danieljebarson21@gmail.com>
2022-12-14 16:20:23 +0530

    Added login page 👍

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import * as React from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App({ Component, pageProps }) {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
