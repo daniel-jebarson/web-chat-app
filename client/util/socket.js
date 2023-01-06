7c6cbc1ae31822ccbe6830b45ff22868de904b59
daniel-jebarson <danieljebarson21@gmail.com>
2023-01-06 17:09:21 +0530

    ⚰️ Removed ununsed code

--- client/util/socket.js
+++
@@ -1,6 +0,0 @@
-// import io from "socket.io-client";
-
-// export const socket = io("ws://localhost:5000/", {
-//   transports: ["websocket"],
-// });
-// // socket.off("MY_EVENT", doThisOnlyOnce).on("MY_EVENT", doThisOnlyOnce);
