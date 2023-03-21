0fdb025bc02fdfffc288c52bf16cd45e25e346b5
daniel-jebarson <danieljebarson21@gmail.com>
2023-03-22 01:43:19 +0530

    🚑 Fix : post request

--- server/middleware/logs/log.js
+++ server/middleware/logs/log.js
@@ -1,6 +1,14 @@
 const colors = require("colors");
 const logHandler = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   res.header("Access-Control-Allow-Headers", "*");
   console.log(
     `${req.method} to ${req.path} ,body= ${JSON.stringify(
