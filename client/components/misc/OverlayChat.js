5d987e4fa7ec7c82fbf907336e2c6b8d0e4c3b37
daniel-jebarson <danieljebarson21@gmail.com>
2023-04-07 13:39:35 +0530

    ✨ Add : white-overlay img

--- client/components/misc/OverlayChat.js
+++ client/components/misc/OverlayChat.js
@@ -1,18 +1,19 @@
-import { Container, Flex, Image } from "@chakra-ui/react";
+import { Container, Flex, Image, useColorModeValue } from "@chakra-ui/react";
 function OverlayChat() {
+  const bgOverlayImage = useColorModeValue(
+    "https://cdn.discordapp.com/attachments/921608169438994443/1093809257251614870/white-overlay.jpg",
+    "https://cdn.discordapp.com/attachments/817021689782468648/1055098433070977035/Dani_Chat_App_-_Overlay.jpg"
+  );
   return (
     <Flex
       flexGrow={"1"}
       m={"0"}
       opacity={"100"}
       saturate={"60"}
-      bgImage={
-        "https://cdn.discordapp.com/attachments/817021689782468648/1055098433070977035/Dani_Chat_App_-_Overlay.jpg"
-      }
+      bgImage={bgOverlayImage}
       bgSize={"cover"}
       bgRepeat={"no-repeat"}
       alignItems={"stretch"}
-      // bgColor="red"
     ></Flex>
   );
 }
